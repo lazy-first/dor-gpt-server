9755d4042afe6a348de3e09f3dd3e3cf0ef5e6e5
lazy-first <shinhb_2@naver.com>
2023-08-21 18:50:19 +0900

    set2

--- server.js
+++ server.js
@@ -1,97 +1,104 @@
-
-// 서버를 띄우기 위한 express 라이브러리 기본세팅
 const express = require('express');
 const cors = require('cors');
 const app = express();
-
-const bodyParser= require('body-parser')
+const bodyParser = require('body-parser');
 const axios = require('axios');
 
-// RIOT API KEY
-const API_KEY = 'RGAPI-bd475f82-111c-402a-96dd-95c8a377bfc8';
+const API_KEY = 'RGAPI-ef2547c1-a931-4771-9402-8aa38492c4df';
 const baseUrl = 'https://KR.api.riotgames.com/lol';
 const baseUrl2 = 'https://asia.api.riotgames.com/lol';
 
 app.use(cors());
 app.use(bodyParser.json());
 
-// listen( 포트번호, 띄운 뒤 실행코드 )
-app.listen(8080, function(){
-    console.log('listening on 8080')
+app.listen(8080, function () {
+  console.log('listening on 8080');
 });
 
-
-
 async function getMatchDetails(matchId, puuid) {
-    try {
-      const matchResponse = await axios.get(`${baseUrl2}/match/v5/matches/${matchId}`, {
-        headers: {
-          'X-Riot-Token': API_KEY,
-        },
-      });
-  
-      const matchData = matchResponse.data;
-  
-      // 매치 데이터에서 필요한 정보 추출
-      const participantIdentities = matchData.info.participants;
-      const participantId = participantIdentities.find(participant => participant.puuid === puuid)?.participantId;
-  
-      const participant = matchData.info.participants.find(participant => participant.participantId === participantId);
-  
-      const kills = participant.kills;
-      const deaths = participant.deaths;
-      const assists = participant.assists;
-      const championName = participant.championName;
-      const win = participant.win;
+  try {
+    const matchResponse = await axios.get(`${baseUrl2}/match/v5/matches/${matchId}`, {
+      headers: {
+        'X-Riot-Token': API_KEY,
+      },
+    });
 
-      // 챔피언 이름에 따른 이미지 url
-      const championImageUrl = `http://ddragon.leagueoflegends.com/cdn/13.15.1/img/champion/${championName}.png`;
-  
-      return { kills, deaths, assists, championName, championImageUrl, win, teams };
-    } catch (error) {
-      console.error(error);
-      return null;
+    const matchData = matchResponse.data;
+
+    // Find the participant with the given puuid
+    const participant = matchData.info.participants.find(participant => participant.puuid === puuid);
+
+    if (!participant) {
+      return null; // Participant not found, handle this case
     }
-  }
 
+    const kills = participant.kills;
+    const deaths = participant.deaths;
+    const assists = participant.assists;
+    const championName = participant.championName;
+    const win = participant.win;
+
+    // Determine the lane the user played
+    const lane = participant.lane;
+
+    // Image URL by champion name
+    const championImageUrl = `http://ddragon.leagueoflegends.com/cdn/13.15.1/img/champion/${championName}.png`;
+
+    // Get team members of the user's team
+    const teamId = participant.teamId;
+    const teamMembers = matchData.info.participants.filter(participant => participant.teamId === teamId);
+
+    // Extract information for team members
+    const teamMemberInfo = teamMembers.map(teamMember => ({
+      championName: teamMember.championName,
+      championImageUrl: `http://ddragon.leagueoflegends.com/cdn/13.15.1/img/champion/${teamMember.championName}.png`,
+      kills: teamMember.kills,
+      deaths: teamMember.deaths,
+      assists: teamMember.assists,
+      lane: teamMember.lane,
+    }));
+
+    return { championName, championImageUrl, kills, deaths, assists, win, lane, teamMembers: teamMemberInfo };
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
 
 app.post('/summoner', async function (req, res) {
-    const summonerName = req.body.name;
-  
+  const summonerName = req.body.name;
+
+  try {
+    // Get summoner information
+    const summonerResponse = await axios.get(`${baseUrl}/summoner/v4/summoners/by-name/${summonerName}`, {
+      headers: {
+        'X-Riot-Token': API_KEY,
+      },
+    });
+
+    const puuid = summonerResponse.data.puuid;
+
     try {
-      const response = await axios.get(`${baseUrl}/summoner/v4/summoners/by-name/${summonerName}`, {
+      // Get the last 4 match IDs
+      const matchResponse = await axios.get(`${baseUrl2}/match/v5/matches/by-puuid/${puuid}/ids`, {
         headers: {
           'X-Riot-Token': API_KEY,
         },
       });
-  
-      const puuid = response.data.puuid;
-  
-    //   console.log(puuid)
 
+      // Last 4 matches
+      const matchIds = matchResponse.data.slice(0, 4);
 
-      try {
-        const matchResponse = await axios.get(`${baseUrl2}/match/v5/matches/by-puuid/${puuid}/ids`, {
-          headers: {
-            'X-Riot-Token': API_KEY,
-          },
-        });
-        
-        
-        // 최근 4개의 경기
-        const matchIds = matchResponse.data.slice(0, 4);
+      // Retrieve details for each match
+      const matchDetails = await Promise.all(matchIds.map(matchId => getMatchDetails(matchId, puuid)));
 
-        const matchDetails = await Promise.all(matchIds.map(matchIds => getMatchDetails(matchIds, puuid)));
-         
-        
-        res.json({ matchDetails });
-      } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: '오류 발생' });
-      }
-  
+      res.json({ matchDetails });
     } catch (error) {
       console.error(error);
-      res.status(500).json({ error: '오류 발생' });
+      res.status(500).json({ error: 'An error occurred' });
     }
-  });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred' });
+  }
+});
